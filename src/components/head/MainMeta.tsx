857f0e2c65f90e6cf40d65655e87723d5e627cf1
WincerChan <WincerChan@gmail.com>
2023-05-07 23:34:43 +0800

    :fire: remove og:article:tag tag

--- src/components/head/MainMeta.tsx
+++ src/components/head/MainMeta.tsx
@@ -1,16 +1,21 @@
 import siteConf from '@/hugo.json';
-import { Link, Meta, Title } from 'solid-start';
+import { Meta } from "@solidjs/meta";
+import { Show } from 'solid-js';
+import { Link, Title } from 'solid-start';
 import { HeadParamsTyoe } from '~/schema/Head';
 
 const MainMeta = ({ params }: { params: HeadParamsTyoe }) => {
     const title = params.title ? `${params.title} · ${siteConf.title}` : siteConf.title
     return (
         <>
+            <Link as='image' href={siteConf.avatar} rel='preload' />
+            <Show when={params.cover}>
+                <Link as='image' href={params.cover} rel='preload' />
+            </Show>
             <Meta charset="utf-8" />
             <Meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
             <Link rel="canonical" href={new URL(params.pageURL, siteConf.baseURL).toString()} />
             <Link rel="preconnect" href={siteConf.cdnURL} />
-            <Link rel="preconnect" href="https://g.alicdn.com" />
             <Title>{title}</Title>
             <Meta name="description" content={params.description || siteConf.description} />
             <Meta name="keywords" content={params.keywords.join(", ")} />
--- src/components/head/OpenGraph.tsx
+++ src/components/head/OpenGraph.tsx
@@ -1,5 +1,4 @@
 import { default as siteConf } from "@/hugo.json";
-import { For } from "solid-js";
 import { Meta } from "solid-start";
 import { HeadParamsTyoe } from "~/schema/Head";
 
@@ -9,12 +8,11 @@ const OpenGraph = ({ params }: { params: HeadParamsTyoe }) => {
         <>
             <Meta property="og:title" content={title} />
             <Meta property="og:type" content="article" />
+            <Meta property="og:site_name" content={siteConf.title} />
+            <Meta property="og:locale" content="zh_CN" />
             <Meta property="og:description" content={params.description || siteConf.description} />
             <Meta property="og:url" content={`${new URL(params.pageURL, siteConf.baseURL)}`} />
             <Meta property="og:image" content="https://ae01.alicdn.com/kf/H3581d2df939f4c3182b1d4b9c2a47bdaO.png" />
-            <For each={params.keywords}>
-                {keyword => <Meta property="og:article:tag" content={keyword} />}
-            </For>
             <Meta property="article:published_time" content={params.date.toISOString()} />
             <Meta property="article:modified_time" content={params.updated.toISOString()} />
             <Meta property="article:author" content={siteConf.author.name} />
