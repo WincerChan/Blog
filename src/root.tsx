eb3e15ff92aa9c1c0c0f449ad4a4c395305a2643
WincerChan <WincerChan@gmail.com>
2023-05-09 08:36:43 +0800

    :chart_with_upwards_trend: update track website id

--- src/root.tsx
+++ src/root.tsx
@@ -36,7 +36,7 @@ export default function Root() {
         <Footer />
         <BackTop />
         <Scripts />
-        {__IS_PROD ? <script async defer data-website-id="635fdaf6-88da-46c2-bbbd-277a77276d7f"
+        {__IS_PROD ? <script async defer data-website-id="d50e9a0c-1726-40f7-b622-7bbf7aa989bc"
           src="https://api.itswincer.com/react/route.js" /> : <></>}
         {__IS_PROD ?
           <script innerHTML={`if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}`} />
