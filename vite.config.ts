e190058c27289be08720ff18127a8d5489f4907b
WincerChan <WincerChan@gmail.com>
2023-05-11 00:11:19 +0800

    :wrench:

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,74 @@ import jsonxPlugin from "./plugin/jsonx";
 import { randomTags, totalPosts, wordsCount } from "./plugin/statsPreload";
 
 const isProd = process.env.NODE_ENV === "production";
+const PWAConfig = {
+  workbox: {
+    cacheId: "wir-cache",
+    globPatterns: [],
+    navigateFallback: null,
+    runtimeCaching: [
+      {
+        urlPattern: /.*\.html.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'wir-blog',
+          cacheableResponse: {
+            statuses: [200]
+          }
+        }
+      },
+      {
+        urlPattern: /\.(?:png|jpg|jpeg|webp)$/,
+        handler: 'CacheFirst',
+        options: {
+          cacheName: 'wir-images',
+          expiration: {
+            maxEntries: 20
+          }
+        }
+      },
+      {
+        urlPattern: /.*\.js.*/,
+        handler: "StaleWhileRevalidate",
+        options: {
+          cacheName: 'wir-js',
+          expiration: {
+            maxEntries: 5,
+            maxAgeSeconds: 7 * 24 * 60 * 60
+          },
+          cacheableResponse: {
+            statuses: [200]
+          }
+        }
+      }
+    ]
+  },
+  manifest: {
+    name: "Wincer's Blog",
+    short_name: "Wincer",
+    start_url: "/",
+    id: "/",
+    display: "fullscreen",
+    orientation: "natural",
+    theme_color: "#065279",
+    background_color: "#065279",
+    icons: [
+      {
+        src: "https://cdn.jsdelivr.net/npm/wir@1.0.2/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png"
+      },
+      {
+        "src": "https://cdn.jsdelivr.net/npm/wir@1.0.2/android-chrome-512x512.png",
+        "sizes": "512x512",
+        "type": "image/png",
+        "purpose": "any maskable"
+      }
+    ]
+  },
+  selfDestroying: false
+
+}
 
 export default defineConfig({
   resolve: {
@@ -26,72 +94,14 @@ export default defineConfig({
     { ...jsonxPlugin(), enforce: "pre" },
     solid({ adapter: staticAdpater(), extensions: [".jsonx"] }),
     UnoCSS(),
-    VitePWA({
-      workbox: {
-        cacheId: "wir-cache",
-        globPatterns: [],
-        navigateFallback: null,
-        runtimeCaching: [
-          {
-            urlPattern: /.*\.html.*/,
-            handler: 'NetworkFirst',
-            options: {
-              cacheName: 'wir-blog',
-              cacheableResponse: {
-                statuses: [200]
-              }
-            }
-          },
-          {
-            urlPattern: /\.(?:png|jpg|jpeg|webp)$/,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'wir-images',
-              expiration: {
-                maxEntries: 20
-              }
-            }
-          },
-          {
-            urlPattern: /.*\.js.*/,
-            handler: "StaleWhileRevalidate",
-            options: {
-              cacheName: 'wir-js',
-              expiration: {
-                maxEntries: 5,
-                maxAgeSeconds: 7 * 24 * 60 * 60
-              },
-              cacheableResponse: {
-                statuses: [200]
-              }
-            }
-          }
-        ]
-      },
-      manifest: {
-        name: "Wincer's Blog",
-        short_name: "Wincer",
-        start_url: "/",
-        id: "/",
-        display: "fullscreen",
-        orientation: "natural",
-        theme_color: "#065279",
-        background_color: "#065279",
-        icons: [
-          {
-            src: "https://cdn.jsdelivr.net/npm/wir@1.0.2/android-chrome-192x192.png",
-            sizes: "192x192",
-            type: "image/png"
-          },
-          {
-            "src": "https://cdn.jsdelivr.net/npm/wir@1.0.2/android-chrome-512x512.png",
-            "sizes": "512x512",
-            "type": "image/png",
-            "purpose": "any maskable"
-          }
-        ]
-      },
-      selfDestroying: false
-    })
+    VitePWA(PWAConfig)
   ],
+  build: {
+    minify: false,
+    rollupOptions: {
+      output: {
+        chunkFileNames: `assets/[name].js`,
+      }
+    }
+  }
 });
