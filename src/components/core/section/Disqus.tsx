ef9f258140bd136a482cd9b16c7ee802223bec90
WincerChan <WincerChan@gmail.com>
2023-06-02 13:09:59 +0800

    :bug: fix load disuqs error

--- src/components/core/section/Disqus.tsx
+++ src/components/core/section/Disqus.tsx
@@ -4,24 +4,25 @@ import { isBrowser } from "~/utils";
 
 const disqusShort = "wincer"
 
-const DisqusButton = ({ nowLoad, setLoad }: { nowLoad: Accessor<unknown>, setLoad: (arg0: boolean) => void }) => {
-    const [visibility, setVisibility] = createSignal(true)
+const DisqusButton = ({ nowLoad, shouldLoad }: { nowLoad: Accessor<unknown>, shouldLoad: (arg0: boolean) => void }) => {
+    const [showButton, setShowButton] = createSignal(true)
     const [errorMsg, setErrorMsg] = createSignal('')
-    const [loading, setLoading] = createSignal(false)
+    const [loadingState, setLoadingState] = createSignal(false)
     const load = () => {
         if (!isBrowser) return
-        if (typeof DISQUS !== "undefined") { setLoad(true); return }
-        setLoading(true)
+        // 之前在其他的页面已经成功加载过 DISUQS，直接返回
+        if (typeof DISQUS !== "undefined") { shouldLoad(true); setShowButton(false); return }
+        setLoadingState(true)
         const d = document, s = d.createElement('script');
         s.src = `https://${disqusShort}.disqus.com/embed.js`;
         s.onerror = () => {
             setErrorMsg("看起来 Disqus 无法加载，可能是因为你的网络环境不太好。")
-            setVisibility(true)
+            setShowButton(true)
         }
         s.onload = () => {
             setErrorMsg("")
-            setLoading(false)
-            setVisibility(false)
+            setLoadingState(false)
+            setShowButton(false)
         }
         s.setAttribute('data-timestamp', `${+new Date()}`);
         (d.head || d.body).appendChild(s);
@@ -33,14 +34,14 @@ const DisqusButton = ({ nowLoad, setLoad }: { nowLoad: Accessor<unknown>, setLoa
 
     return (
         <>
-            <div id="disqus_thread" class={`w-full ${!visibility() ? 'min-h-64' : ''}`}></div>
+            <div id="disqus_thread" class={`w-full ${!showButton() ? 'min-h-64' : ''}`}></div>
             <Show when={errorMsg()}>
                 <p class="mb-4"><b>{errorMsg()}</b></p>
             </Show>
-            <Show when={loading()}>
+            <Show when={loadingState()}>
                 <p>正在尝试加载 Disqus，请稍等</p>
             </Show>
-            <Show when={!loading() && visibility()}>
+            <Show when={!loadingState() && showButton()}>
                 <button onClick={load} title="点击加载评论" class=":: p-3 font-headline w-full leading-8 rounded card-outline ">点击以加载 Disqus 评论</button>
             </Show>
         </>
@@ -56,13 +57,17 @@ const DisqusComment = ({ slug }: { slug: string }) => {
     createEffect(() => {
         const observer = new IntersectionObserver(entries => {
             if (entries[0].isIntersecting) {
+                window.disqus_config = function () {
+                    this.page.url = `${pageCanonical}`;
+                    this.page.identifier = `${pageCanonical}`;
+                }
                 const img = new Image();
                 img.src = 'https://disqus.com/favicon.ico?' + new Date().getTime(); // 添加时间戳防止缓存
                 img.onload = function () {
                     visible() ?? setVisible(true)
                 };
                 typeof umami !== "undefined" && umami.track(`Completion ${slug} reading`)
-                setTimeout(() => { visible() ?? setVisible(false); console.log("100ms 内无法连接到 disqus。") }, 100)
+                setTimeout(() => { visible() ?? (setVisible(false), console.log("100ms 内无法连接到 disqus。")) }, 100)
                 observer.unobserve(entries[0].target)
             }
         })
@@ -75,19 +80,12 @@ const DisqusComment = ({ slug }: { slug: string }) => {
                 this.page.url = pageCanonical;
                 this.page.identifier = pageCanonical;
             }
-        });
+        })
     })
     const pageCanonical = new URL(slug, siteConf.baseURL)
     return (
         <div class=":: mt-8 <md:mx-4 <md:mb-8 " ref={self!}>
-            <script id="disqus_config" innerHTML={`
-                var disqus_config = function () {
-                    this.page.url = "${pageCanonical}" ;
-                    this.page.identifier = "${pageCanonical}";
-                };
-            `
-            } />
-            <DisqusButton nowLoad={visible} setLoad={setVisible} />
+            <DisqusButton nowLoad={visible} shouldLoad={setVisible} />
         </div>
     )
 
