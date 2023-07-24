dc1388d46fe6ddb5e3a42d9cc3f7317feadd6941
WincerChan <WincerChan@gmail.com>
2023-07-24 23:29:18 +0800

    :zap: update serviceWorker version

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -5,35 +5,40 @@ skipWaiting();
 clientsClaim();
 
 const ASSETS_PREFIEXES = [
-    `https://unpkg.com/wir@${import.meta.env.VITE_ASSET_VERSION}`,
-    `https://npm.onmicrosoft.cn/wir@${import.meta.env.VITE_ASSET_VERSION}`,
+    `https://unpkg.com/wir@`,
+    `https://npm.onmicrosoft.cn/wir@`,
     ``
 ]
 
 
+const fetchAsset = (url: string, signal) => {
+    return new Promise((resolve, reject) => {
+        fetch(url, { signal })
+            .then(async res => res.ok ? resolve(res) : reject())
+            .catch(() => reject())
+    })
+}
 
-const fetchAssets = async (pathname: string) => {
+
+const fetchAssets = async (pathname: string, version: string) => {
     const controller = new AbortController(),
         signal = controller.signal;
-    return Promise.any(ASSETS_PREFIEXES.map(prefix => fetch(`${prefix}${pathname}`, { signal })))
+    return Promise.any(ASSETS_PREFIEXES.map(prefix => fetchAsset(!prefix ? pathname : `${prefix}${version}${pathname}`, signal)))
         .then(async res => {
-            if (res.status !== 200) Promise.reject(res.status);
-            const text = await res.text();
+            const body = await res.text();
             controller.abort();
-            return {
-                headers: res.headers,
-                body: text,
-                status: res.status,
-            }
+            return { headers: res.headers, body: body, status: res.status }
         })
-        .catch(err => Promise.reject(err))
+        .catch(err => console.log(err))
+
 }
 
 // 添加自定义的路由和策略
-registerRoute(({ request }) => request.url.includes("/assets/") && (request.destination === 'script' || request.destination === 'style'),
+registerRoute(({ request }) => request.url.includes("assets") && (request.destination === 'script' || request.destination === 'style'),
     async ({ event }) => {
+        const version = event.target.location.search.slice(3)
         const parsedUrl = new URL(event.request.url);
-        const { body, ...rest } = await fetchAssets(parsedUrl.pathname)
+        const { body, ...rest } = await fetchAssets(parsedUrl.pathname, version)
         return new Response(body, rest)
     }
 );
