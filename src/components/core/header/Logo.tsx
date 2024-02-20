6c5a1e827d10b45bff10a11c7f081f29dee23622
WincerChan <WincerChan@gmail.com>
2024-02-20 17:45:29 +0800

    :bug: fix avatar not show

--- src/components/core/header/Logo.tsx
+++ src/components/core/header/Logo.tsx
@@ -4,7 +4,7 @@ import { A } from "solid-start";
 const Logo = () => {
     return (
         <A href="/" activeClass="" class=":: flex items-center justify-center bg-menuHover trans-linear cursor-pointer space-x-3 h-menu <sm:h-14 " >
-            <img src={__SITE_CONF.avatar} alt="avatar" id="avatar" width={32} height={32} class=":: rounded-36 sm:w-9 sm:h-9 shadow-round " />
+            <img src={__SITE_CONF.avatar} referrerpolicy="no-referrer" alt="avatar" id="avatar" width={32} height={32} class=":: rounded-36 sm:w-9 sm:h-9 shadow-round " />
             <h1 class=":: text-[22px] subpixel-antialiased font-medium font-headline ">
                 Wincer's Blog
             </h1>
