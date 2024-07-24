bac61fc43c3975bfd6cc6852146537d8bf284fee
WincerChan <WincerChan@gmail.com>
2024-07-25 00:29:29 +0800

    fix toc auto-focus

--- src/components/core/sidebar/ToC.tsx
+++ src/components/core/sidebar/ToC.tsx
@@ -1,4 +1,5 @@
-import { createSignal, onMount } from "solid-js";
+import { useLocation } from "@solidjs/router";
+import { createEffect, createMemo, createSignal, onMount } from "solid-js";
 import { useI18nContext } from "~/i18n/i18n-solid";
 import IconCatalog from "~icons/carbon/catalog";
 import { globalStore } from "../header/ThemeSwitch/Provider";
@@ -9,23 +10,84 @@ interface ToCInterface {
     slug: string,
 }
 const ToC = ({ toc, slug }: ToCInterface) => {
-    const { LL, locale } = useI18nContext()
+    let ref: HTMLDivElement;
+    const { LL } = useI18nContext()
     const [visible, setVisible] = createSignal(false);
+    const [scrolling, setScrolling] = createSignal(false)
+    const [tocIndex, setTocIndex] = createSignal(-1)
+    const [hIndex, setHIndex] = createSignal(-1)
+    createEffect(() => {
+        if (tocIndex() == -1) return
+        const link = ref.getElementsByTagName('a')[tocIndex()]
+        ref.scrollTop = link.offsetTop - 57 - 36
+    })
+    createEffect(() => {
+        if (scrolling()) return
+        if (hIndex() == -1) return
+        const link = ref.getElementsByTagName('a')[hIndex()]
+        ref.scrollTop = link.offsetTop - 57 - 36
+    })
+
     if (toc === '<nav id="TableOfContents"></nav>') return <></>
     if (!toc) return <></>
-    const [readingProgress, setReadingProgress] = createSignal(0);
-    const scrollListerner = (initOffset, sectionTarget) => {
-        const windowScrollTop = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0
-        const scrollHeight = windowScrollTop - initOffset
-        const normalizedHeight = Math.min(Math.max(0, scrollHeight), sectionTarget.offsetHeight)
-        setReadingProgress(Math.floor(normalizedHeight / sectionTarget.offsetHeight * 100))
-    }
-    onMount(() => {
-        const sectionTarget = document.getElementById('blog-article')
-        if (!sectionTarget) return
-        const offset = sectionTarget.offsetTop - Math.floor(window.innerHeight / 1.6)
-        window.addEventListener("scroll", () => scrollListerner(offset, sectionTarget))
+    const loc = useLocation()
+    const hash = createMemo(() => loc.hash)
+    createEffect(() => {
+        if (!hash()) return
+        const elems = ref.getElementsByTagName('a')
+        Array.from(elems).forEach((element, i) => {
+            element.onclick = () => {
+                setTocIndex(i)
+                setScrolling(true)
+                setTimeout(() => setScrolling(false), 500)
+            }
+            if (element.href.endsWith(hash())) {
+                element.classList.add('active')
+            } else {
+                element.classList.remove('active')
+            }
+        });
     })
+    onMount(() => {
+        const sections = document.querySelectorAll("#blog-article h2, h3, h4");
+        const tocLinks = document.querySelectorAll("#TableOfContents a");
+        const options = {
+            root: null,
+            rootMargin: "0px 0px -90% 0px",
+            threshold: 0 // 50%的可见度
+        };
+        document.addEventListener("scroll", () => {
+            if (hIndex() == -1) return
+            if (document.documentElement.scrollTop === sections[hIndex()].offsetTop) {
+                console.log("scroll right")
+                setTimeout(() => { setScrolling(false) }, 100)
+            }
+        })
+        const callback = (entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    tocLinks.forEach((link, i) => {
+                        link.classList.remove("active");
+                        if (link.getAttribute("href").substring(1) === entry.target.id) {
+                            setHIndex(i)
+                            link.classList.add("active");
+                        }
+                    });
+                }
+            });
+        };
+
+        const observer = new IntersectionObserver(callback, options);
+
+        sections.forEach(section => {
+            observer.observe(section);
+        });
+    })
+
+    const ClickToC = (event) => {
+        setVisible(false);
+        globalStore.trackEvent("Click TableofContents", { props: { slug: slug } })
+    }
     return (
         <>
             <aside class=":: lg:max-w-80 lg:ml-6 <lg:fixed <lg:z-10 ">
@@ -37,41 +99,11 @@ const ToC = ({ toc, slug }: ToCInterface) => {
                     </Modal>
                     <div id="toc" class={`:: duration-200 z-20 overflow-y-auto transition-max-height toc-modal ${visible() ? '<lg:max-h-42vh' : '<lg:max-h-0'} `}>
                         <div class=":: h-4 "></div>
-                        <div class=":: ml-2 flex font-headline "><label>{LL().sidebar.TOC()}</label> <span class=":: mt-1px ">（{readingProgress()}%）</span></div>
-                        <div onClick={() => { setVisible(false); globalStore.trackEvent("Click TableofContents", { props: { slug: slug } }) }} class=":: mt-2 mb-4 flex-wrap flex overflow-y-auto max-h-41vh " innerHTML={toc} />
+                        <div class=":: ml-2 flex font-headline "><label>{LL().sidebar.TOC()}</label> </div>
+                        <div ref={ref} onClick={ClickToC} class=":: mt-2 mb-4 flex-wrap flex overflow-y-auto max-h-42vh scroll-smooth " innerHTML={toc} />
                     </div>
                 </div>
             </aside >
-            <script innerHTML={`
-            const sections = document.querySelectorAll("article h2, h3, h4");
-    const tocLinks = document.querySelectorAll("#TableOfContents a");
-
-const options = {
-        root: null,
-        rootMargin: "0px 0px -80% 0px",
-        threshold: 0 // 50%的可见度
-    };
-
-    const callback = (entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                tocLinks.forEach(link => {
-                    link.classList.remove("active");
-                    if (link.getAttribute("href").substring(1) === entry.target.id) {
-                        link.classList.add("active"); console.log("kjkffj")
-                    }
-                });
-            }
-        });
-    };
-
-    const observer = new IntersectionObserver(callback, options);
-
-    sections.forEach(section => {
-    console.log("fjkjkf")
-        observer.observe(section);
-    });
-                `} />
         </>
     )
 }
