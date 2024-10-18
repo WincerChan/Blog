17687ebe354171f2f74e3406e5b9eda09809b032
WincerChan <WincerChan@gmail.com>
2024-10-18 17:06:33 +0800

    :art: update code for new version hugo

--- src/components/page/Friends.tsx
+++ src/components/page/Friends.tsx
@@ -1,24 +1,17 @@
 import friendLinks from "@/hugo/content/friends.json";
-import { For } from 'solid-js';
-import FriendLink from '~/components/core/section/FriendLink';
-import PostLayout from '../layouts/PostLayout';
-
+import { For } from "solid-js";
+import FriendLink from "~/components/core/section/FriendLink";
+import PostLayout from "../layouts/PostLayout";
 
 const Friend = ({ page, children }) => {
-    return (
-        <PostLayout rawBlog={page}>
-            {children}
-            <div class=':: grid grid-cols-1 gap-6 my-6 sm:grid-cols-2 '>
-                <For each={friendLinks}>
-                    {
-                        link => <FriendLink {...link} />
-                    }
-                </For>
-            </div>
-        </PostLayout>
-    )
-}
-
+  return (
+    <PostLayout rawBlog={page}>
+      {children}
+      <div class=":: grid grid-cols-1 gap-6 my-6 sm:grid-cols-2 ">
+        <For each={friendLinks}>{(link) => <FriendLink {...link} />}</For>
+      </div>
+    </PostLayout>
+  );
+};
 
 export default Friend;
-
