85997f6481d399d5cd6db24f0af63028822a1648
WincerChan <WincerChan@gmail.com>
2025-03-06 18:04:13 +0800

    :lipstick: remove class from body to div

--- plugin/swBuild.ts
+++ plugin/swBuild.ts
@@ -3,24 +3,24 @@ import { build } from "vite";
 
 const SwBuild = (dirname) => {
     return {
-        name: 'vite-plugin-sw-build',
-        apply: 'build',
+        name: "vite-plugin-sw-build",
+        apply: "build",
         closeBundle() {
             build({
                 configFile: false,
                 build: {
-                    outDir: resolve(dirname, ".output/public"),
+                    outDir: resolve(dirname, "public"),
                     emptyOutDir: false,
                     rollupOptions: {
                         input: resolve(dirname, "src/service-worker.ts"),
                         output: {
-                            entryFileNames: "sw.js"
-                        }
-                    }
-                }
-            })
-        }
-    }
-}
+                            entryFileNames: "sw.js",
+                        },
+                    },
+                },
+            });
+        },
+    };
+};
 
-export default SwBuild
+export default SwBuild;
--- src/app.tsx
+++ src/app.tsx
@@ -6,50 +6,54 @@ import Footer from "./components/core/footer";
 import Header from "./components/core/header";
 import TypesafeI18n from "./i18n/i18n-solid";
 
-import { type RouteSectionProps } from '@solidjs/router';
-import { globalStore, setGlobalStore } from "./components/core/header/ThemeSwitch/Provider";
+import { type RouteSectionProps } from "@solidjs/router";
+import {
+    globalStore,
+    setGlobalStore,
+} from "./components/core/header/ThemeSwitch/Provider";
 import { Locale } from "./utils/locale";
 
 import Plausible from "plausible-tracker";
 import { isServer } from "solid-js/web";
 import { loadLocale } from "./i18n/i18n-util.sync";
-let PlausibleTracker = 'default' in Plausible ? Plausible['default'] : Plausible;
+let PlausibleTracker =
+    "default" in Plausible ? Plausible["default"] : Plausible;
 const { trackPageview, trackEvent } = PlausibleTracker({
-  domain: "blog.itswincer.com",
-  apiHost: "https://track.itswincer.com",
-})
-
+    domain: "blog.itswincer.com",
+    apiHost: "https://track.itswincer.com",
+});
 
 const preloadHook = (props: RouteSectionProps<unknown>) => {
-  return <MetaProvider>
-    <Suspense>{props.children}</Suspense>
-  </MetaProvider>
-}
+    return (
+        <MetaProvider>
+            <Suspense>{props.children}</Suspense>
+        </MetaProvider>
+    );
+};
 
 export default function App() {
-  let locale: Locale = "zh-CN";
-  if (isServer) {
-    globalThis.renderCount ??= 0;
-    if (++globalThis.renderCount < __EN_POSTS.length)
-      locale = "en"
-    setGlobalStore({ locale: locale })
-    loadLocale(locale)
-  }
-  createEffect(() => {
-    document.documentElement.className = globalStore.theme
-  })
-  onMount(() => {
-    setGlobalStore({ trackEvent: trackEvent, trackPage: trackPageview })
-  })
-  return (
-    <TypesafeI18n locale={globalStore.locale as Locale}>
-      <Header />
-      <Router
-        root={props => preloadHook(props)}
-      >
-        <FileRoutes />
-      </Router >
-      <Footer />
-    </TypesafeI18n>
-  );
+    let locale: Locale = "zh-CN";
+    if (isServer) {
+        globalThis.renderCount ??= 0;
+        if (++globalThis.renderCount < __EN_POSTS.length) locale = "en";
+        setGlobalStore({ locale: locale });
+        loadLocale(locale);
+    }
+    createEffect(() => {
+        document.documentElement.className = globalStore.theme;
+    });
+    onMount(() => {
+        setGlobalStore({ trackEvent: trackEvent, trackPage: trackPageview });
+    });
+    return (
+        <TypesafeI18n locale={globalStore.locale as Locale}>
+            <div class=":: font-base antialiased bg-main text-main md:grid md:min-h-screen grid-rows-[auto_1fr_auto] ">
+                <Header />
+                <Router root={(props) => preloadHook(props)}>
+                    <FileRoutes />
+                </Router>
+                <Footer />
+            </div>
+        </TypesafeI18n>
+    );
 }
