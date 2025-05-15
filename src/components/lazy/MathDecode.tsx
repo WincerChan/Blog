7b6c6731e4220a4b73b1b26b8122ebd17f1bf1a1
WincerChan <WincerChan@gmail.com>
2025-05-15 19:43:00 +0800

    fix math always render to block

--- src/components/lazy/MathDecode.tsx
+++ src/components/lazy/MathDecode.tsx
@@ -3,7 +3,7 @@ import { base64ToUtf8 } from "~/utils/base64";
 
 const MathDecode = ({ children }) => {
     const code = base64ToUtf8(children);
-    return <div class="<md:px-4 <md:max-w-screen" innerHTML={code} />;
+    return <span class="<md:px-4 <md:max-w-screen" innerHTML={code} />;
 };
 
 export default MathDecode;
