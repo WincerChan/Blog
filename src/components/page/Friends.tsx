e36b0740636980994d4de2bd0bed475fabf96b3f
WincerChan <WincerChan@gmail.com>
2025-09-11 12:32:10 +0800

    :sparkles: feat(links): add inactive state and sort to end for disconnected friend links
    - Add `inactive` boolean field to mark disconnected links
    - Implement visual styling with grayscale, opacity and strike-through for inactive links
    - Sort inactive links to the end of the list to prioritize active connections
    - Maintain original order within active and inactive groups respectively

--- src/components/page/Friends.tsx
+++ src/components/page/Friends.tsx
@@ -4,14 +4,19 @@ import FriendLink from "~/components/core/section/FriendLink";
 import PostLayout from "../layouts/PostLayout";
 
 const Friend = ({ page, children }) => {
-  return (
-    <PostLayout rawBlog={page}>
-      {children}
-      <div class=":: grid grid-cols-1 gap-6 my-6 sm:grid-cols-2 ">
-        <For each={friendLinks}>{(link) => <FriendLink {...link} />}</For>
-      </div>
-    </PostLayout>
-  );
+    const activeLinks = friendLinks.filter((link) => !link.inactive);
+    const inactiveLinks = friendLinks.filter((link) => link.inactive === true);
+    const sortedLinks = activeLinks.concat(inactiveLinks);
+    return (
+        <PostLayout rawBlog={page}>
+            {children}
+            <div class=":: grid grid-cols-1 gap-6 my-6 sm:grid-cols-2 ">
+                <For each={sortedLinks}>
+                    {(link) => <FriendLink {...link} />}
+                </For>
+            </div>
+        </PostLayout>
+    );
 };
 
 export default Friend;
