63c63849598a2aec79f3b3c974d3574c16b849f5
WincerChan <WincerChan@gmail.com>
2025-12-29 22:17:45 +0800

    :recycle: refactor(style): override sans font stack

--- src/styles/unocss/theme.ts
+++ src/styles/unocss/theme.ts
@@ -1,1 +1,14 @@
-export const theme = {} as const;
+export const theme = {
+    fontFamily: {
+        sans: [
+            "-apple-system",
+            "BlinkMacSystemFont",
+            "Segoe UI",
+            "Helvetica",
+            "Arial",
+            "sans-serif",
+            "Apple Color Emoji",
+            "Segoe UI Emoji",
+        ],
+    },
+} as const;
