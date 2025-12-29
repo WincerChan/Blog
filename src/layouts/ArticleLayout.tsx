6328d0c07ecf9f5ce94d88ca35b5393cffecf535
WincerChan <WincerChan@gmail.com>
2025-12-29 13:02:06 +0800

    :sparkles: feat(layout): widen toc column for desktop

--- src/layouts/ArticleLayout.tsx
+++ src/layouts/ArticleLayout.tsx
@@ -18,7 +18,7 @@ const ArticleLayout = (props: ArticleLayoutProps) => {
     return (
         <AppLayout lang={resolved().lang}>
             <HeadTag headParams={props.headParams} />
-            <div class="w-full max-w-5xl mx-auto px-4 md:px-0 md:grid md:grid-cols-[1fr_minmax(0,42rem)_1fr] md:gap-x-8">
+            <div class="w-full max-w-7xl mx-auto px-4 md:px-0 md:grid md:grid-cols-[minmax(0,16rem)_minmax(0,42rem)_minmax(0,16rem)] md:gap-x-8">
                 <ToC toc={resolved().toc ?? ""} slug={resolved().pageURL} />
                 <article class="w-full max-w-2xl mx-auto md:col-start-2 md:row-start-1">
                     {props.children}
